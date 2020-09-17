9107ed8511f5fba3c2c5ab02c0808c778a6713bb
CharLemAznable <545541819@qq.com>
2020-09-18 04:24:55 +0800

    Fixed code smell, some refactor.

--- src/main/java/com/github/charlemaznable/vertx/diamond/DiamondIgniteClusterManager.java
+++ src/main/java/com/github/charlemaznable/vertx/diamond/DiamondIgniteClusterManager.java
@@ -3,8 +3,6 @@ package com.github.charlemaznable.vertx.diamond;
 import io.vertx.spi.cluster.ignite.IgniteClusterManager;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.apache.ignite.internal.IgnitionEx;
-import org.apache.ignite.internal.util.typedef.F;
 import org.n3r.diamond.client.cache.ParamsAppliable;
 
 import java.io.ByteArrayInputStream;
@@ -12,6 +10,8 @@ import java.io.ByteArrayInputStream;
 import static com.github.charlemaznable.vertx.diamond.VertxDiamondElf.getVertxClusterConfigStoneByApplyParams;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.apache.ignite.internal.IgnitionEx.loadConfigurations;
+import static org.apache.ignite.internal.util.lang.GridFunc.first;
 import static org.joor.Reflect.on;
 
 @Slf4j
@@ -22,10 +22,10 @@ public class DiamondIgniteClusterManager extends IgniteClusterManager implements
         val configStone = getVertxClusterConfigStoneByApplyParams(params);
         if (isBlank(configStone)) return; // none diamond or blank diamond or error params
 
-        // read diamond stone as XML or YAML format
+        // read diamond stone
         val inputStream = new ByteArrayInputStream(configStone.getBytes(UTF_8));
         try {
-            val cfg = F.first(IgnitionEx.loadConfigurations(inputStream).get1());
+            val cfg = first(loadConfigurations(inputStream).get1());
             on(this).set("cfg", cfg).call("setNodeID", cfg);
         } catch (Exception e) {
             log.error("Failed to set config", e);
